60c26ea1926648d332e96d8b1c19d70efeb9e5f8
Russell Poldrack <poldrack@gmail.com>
2024-10-02 09:22:17 -0700

    refactor: modularize, clean up

--- infomap_sims.py
+++ infomap_sims.py
@@ -14,7 +14,6 @@
 
 # %%
 import scipy.io
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from infomap import Infomap
@@ -41,83 +40,24 @@ def z_to_r(z):
     return (np.exp(2.0 * z) - 1) / (np.exp(2.0 * z) + 1)
 
 
+# functions for simulation
+def load_priors(infile="priors.mat", size_ratio=2):
+    mat = scipy.io.loadmat(infile)
 
-# %%
-# load data from matlab file
+    # limit size for computational reasons - only use 1/size_ratio of data
+    sizelimit = int(mat["Priors"][0][0][0].shape[0] / size_ratio)
 
-infile = "priors.mat"
-
-mat = scipy.io.loadmat(infile)
-
-# limit size for computational reasons - only use first half of data
-sizelimit = int(mat["Priors"][0][0][0].shape[0] / 2)
-fcpriors = mat["Priors"][0][0][0][:sizelimit]
-spatialpriors = mat["Priors"][0][0][1][:sizelimit]
-names = [str(i[0][0]) for i in mat["Priors"][0][0][2]]
-rgbvals = mat["Priors"][0][0][3]
-
-# %%
-plt.imshow(fcpriors, cmap="hot", interpolation="nearest", aspect="auto")
-
-# %%
-maxprob_fc = np.argmax(fcpriors, axis=1).astype(int)
-maxprob_sp = np.argmax(spatialpriors, axis=1).astype(int)
-
-# compute jaccard between maxprob_fc and maxprob_sp
-jaccard = np.sum(maxprob_fc == maxprob_sp) / len(maxprob_fc)
-
-jaccard
-
-# %%
-df = pd.DataFrame({"maxprob_fc": maxprob_fc, "maxprob_sp": maxprob_sp})
+    fcpriors = mat["Priors"][0][0][0][:sizelimit]
+    spatialpriors = mat["Priors"][0][0][1][:sizelimit]
+    names = [str(i[0][0]) for i in mat["Priors"][0][0][2]]
+    return mat, fcpriors, spatialpriors, names
 
 
-# %%
-fc_counts = df.maxprob_fc.value_counts().sort_index()
-fc_counts_norm = fc_counts / np.sum(np.isfinite(fcpriors), axis=0)
-sp_counts = df.maxprob_sp.value_counts().sort_index()
-sp_counts_norm = sp_counts / np.sum(np.isfinite(spatialpriors), axis=0)
-
-count_df = pd.DataFrame({"fc_norm": fc_counts_norm, "sp_norm": sp_counts_norm})
-
-ax = count_df.plot(kind="barh", stacked=False)
-ax.set_ylabel("Networks")  # Add your desired x-axis label here
-_ = ax.set_yticklabels(
-    names[:20], ha="right"
-)  # Add tick labels with rotation and alignment
-ax.set_xlabel("Proportion of vertices")
-
-# %%
-# generate simulated affinity matrix based on the fc priors
-
-matching_matrix = (maxprob_fc[:, None] == maxprob_fc).astype(np.float32)
-# set diagonal to zero
-np.fill_diagonal(matching_matrix, 0)
-
-# create a z-scored version for use in generating noisy versions
-matching_matrix_z = r_to_z(matching_matrix)
-
-np.mean(matching_matrix)
-
-# %%
-thresh = .99
-match_pairs = np.where(matching_matrix >= thresh)
-match_pairs_clean = [
-    (int(i), int(j)) for i, j in zip(match_pairs[0], match_pairs[1]) if i != j
-]
-del match_pairs
-
-# %%
-len(match_pairs_clean)
+def get_maxprob_labels(priors):
+    return np.argmax(priors, axis=1).astype(int)
 
 
-# %% [markdown]
-# ### Infomap
-
-# %%
-# create a graph from the matching matrix
-
-def matching_matrix_to_graph(matching_matrix, density=.05):
+def matching_matrix_to_graph(matching_matrix, density=0.05):
     G = nx.Graph()
     G.add_nodes_from(range(matching_matrix.shape[0]))
     thresh = np.percentile(matching_matrix, 100 * (1 - density))
@@ -128,74 +68,99 @@ def matching_matrix_to_graph(matching_matrix, density=.05):
     G.add_edges_from(match_pairs_clean)
     return G
 
-# G = matching_matrix_to_graph(matching_matrix, density=.05)
+
+def get_matching_matrix(maxprob_fc):
+    matching_matrix = (maxprob_fc[:, None] == maxprob_fc).astype(np.float32)
+    # set diagonal to zero
+    np.fill_diagonal(matching_matrix, 0)
+
+    # create a z-scored version for use in generating noisy versions
+    matching_matrix_z = r_to_z(matching_matrix)
+
+    return matching_matrix, matching_matrix_z
 
 
+def get_match_pairs(matching_matrix, thresh=0.99):
+    match_pairs = np.where(matching_matrix >= thresh)
+    match_pairs_clean = [
+        (int(i), int(j)) for i, j in zip(match_pairs[0], match_pairs[1]) if i != j
+    ]
+    return match_pairs_clean
 
-# %%
-def run_infomap(G, true_labels, verbose=False):
+
+def run_infomap(G, fcpriors, verbose=False):
     im = Infomap(silent=True)
 
-    mapping = im.add_networkx_graph(G)
+    _ = im.add_networkx_graph(G)
     im.run()
     if verbose:
         print(f"Found {im.num_top_modules} modules with codelength: {im.codelength}")
 
     module_id_dict = {node.node_id: node.module_id for node in im.tree if node.is_leaf}
     module_list = np.array([module_id_dict[i] for i in range(fcpriors.shape[0])])
+    true_labels = get_maxprob_labels(fcpriors)
     cm = confusion_matrix(true_labels, module_list)
+
+    # relabel modules to match true labels
     cm_argmax = cm.argmax(axis=0)
     module_list_relabeled = np.array([cm_argmax[i] for i in module_list])
 
     return module_list, module_list_relabeled
 
 
-#module_list, module_list_relabeled = run_infomap(G, maxprob_fc, verbose=True)
-#adjusted_rand_score(maxprob_fc, module_list)
-
-# %%
-# add noise to the matching matrix
-# cutoff is the maximum value for the true correlation
-
-def create_noisy_matching_matrix(matching_matrix_z, noise_level=1, cutoff=0.6, seed=None):
+def create_noisy_matching_matrix(
+    matching_matrix_z, noise_level=1, cutoff=0.6, seed=None
+):
     rng = np.random.default_rng(seed)
     noise = rng.normal(0, noise_level, matching_matrix.shape)
     cutoff_z = r_to_z(cutoff)[0]
-    matching_matrix_z_noisy = np.clip(matching_matrix_z, -1 * cutoff_z, cutoff_z) + noise
+    matching_matrix_z_noisy = (
+        np.clip(matching_matrix_z, -1 * cutoff_z, cutoff_z) + noise
+    )
     matching_matrix_noisy = z_to_r(matching_matrix_z_noisy)
     return matching_matrix_noisy
 
-#matching_matrix_noisy = create_noisy_matching_matrix(matching_matrix_z, noise_level=.3)
-#G = matching_matrix_to_graph(matching_matrix_noisy, density=.05)
 
-#module_list = run_infomap(G, verbose=True)
-#adjusted_rand_score(maxprob_fc, module_list)
-
-
-
-# %%
 def get_module_counts(module_list, true_labels):
     modules = sorted(np.unique(true_labels).tolist())
-    counter = {i:int(np.sum(module_list == i)) for i in modules}
+    counter = {i: int(np.sum(module_list == i)) for i in modules}
     return counter
 
-# get_module_counts(module_list, maxprob_fc)
 
+if __name__ == "__main__":
+    nruns = 1
+    size_ratio = 8
 
-# %%
+    mat, fcpriors, spatialpriors, names = load_priors(size_ratio=size_ratio)
 
-results = []
-nruns = 100
+    # we just use functional connectivity priors from WashU team
+    # to create ground truth for this simulation
+    maxprob_fc = get_maxprob_labels(fcpriors)
 
-for noise_level in np.arange(0.05, 0.4, 0.1):
-    for run in range(nruns):
-        matching_matrix_noisy = create_noisy_matching_matrix(matching_matrix_z, noise_level=noise_level)
-        G = matching_matrix_to_graph(matching_matrix_noisy, density=.05)
-        module_list, module_list_relabeled = run_infomap(G, maxprob_fc, verbose=False)
-        ari = adjusted_rand_score(maxprob_fc, module_list)
-        results.append([noise_level, ari] + list(get_module_counts(module_list_relabeled, maxprob_fc).values()))
-        print(f"Noise level: {noise_level}, ARI: {ari}")
+    matching_matrix, matching_matrix_z = get_matching_matrix(maxprob_fc)
 
-# %%
-results_df = pd.DataFrame(results, columns=["noise_level", "ari"] + [f'size_mod{i}' for i in list(get_module_counts(module_list_relabeled, maxprob_fc).keys())])
-results_df.to_csv('results.csv')
+    results = []
+
+    for noise_level in np.arange(0.05, 0.4, 0.1):
+        for run in range(nruns):
+            matching_matrix_noisy = create_noisy_matching_matrix(
+                matching_matrix_z, noise_level=noise_level
+            )
+            G = matching_matrix_to_graph(matching_matrix_noisy, density=0.05)
+            module_list, module_list_relabeled = run_infomap(G, fcpriors, verbose=False)
+            ari = adjusted_rand_score(maxprob_fc, module_list)
+            results.append(
+                [noise_level, ari]
+                + list(get_module_counts(module_list_relabeled, maxprob_fc).values())
+            )
+            print(f"Noise level: {noise_level}, ARI: {ari}")
+
+    results_df = pd.DataFrame(
+        results,
+        columns=["noise_level", "ari"]
+        + [
+            f"size_mod{i}"
+            for i in list(get_module_counts(module_list_relabeled, maxprob_fc).keys())
+        ],
+    )
+    results_df.to_csv("results.csv")
