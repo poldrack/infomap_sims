19eed4d9558ecbfb28ae5e8185e0320e6c416e88
Russell Poldrack <poldrack@gmail.com>
2025-03-05 07:24:15 -0800

    add normalize option

--- infomap_sims.py
+++ infomap_sims.py
@@ -16,7 +16,7 @@
 import argparse
 import os
 import sys
-
+import numpy as np
 
 # utility functions
 def r_to_z(r):
@@ -86,7 +86,16 @@ def get_match_pairs(matching_matrix, thresh=0.99):
     return match_pairs_clean
 
 
-def run_infomap(G, fcpriors, verbose=False):
+# from Daniel Edler:
+# https://github.com/mapequation/poldrack/blob/main/Poldrack%20infomap_sims%20analysis.ipynb
+def normalize_confusion_matrix(cm, module_sizes):
+        norm = np.ones(cm.shape[0])
+        for i, m in enumerate(module_sizes):
+            norm[i] = m
+        return cm / np.outer(norm, np.ones(norm.shape))
+
+
+def run_infomap(G, fcpriors, verbose=False, normalize=False):
     im = Infomap(silent=True)
 
     _ = im.add_networkx_graph(G)
@@ -97,7 +106,7 @@ def run_infomap(G, fcpriors, verbose=False):
     module_id_dict = {node.node_id: node.module_id for node in im.tree if node.is_leaf}
     module_list = np.array([module_id_dict[i] for i in range(fcpriors.shape[0])])
     true_labels = get_maxprob_labels(fcpriors)
-    cm = confusion_matrix(true_labels, module_list)
+    cm = confusion_matrix(true_labels, module_list, normalize=normalize)
 
     # relabel modules to match true labels
     cm_argmax = cm.argmax(axis=0)
@@ -120,7 +129,7 @@ def create_noisy_matching_matrix(
 
 
 def get_module_counts(module_list, true_labels):
-    modules = sorted(np.unique(true_labels).tolist())
+    modules = sorted(np.unique(np.hstack((true_labels, module_list))).tolist())
     counter = {i: int(np.sum(module_list == i)) for i in modules}
     return counter
 
@@ -133,6 +142,7 @@ if __name__ == "__main__":
     parser.add_argument('--noise_level', type=float, default=0.1, help='Noise level for the simulation')
     parser.add_argument('--density', type=float, default=0.05, help='Density of the graph')
     parser.add_argument('--label', type=str, default=None, help='Label for the simulation')
+    parser.add_argument('--normalize', action='store_true', help='Normalize confusion matrix')
     args = parser.parse_args()
 
     filename = f"sim_results/infomap_sim_{args.noise_level:02f}_{args.label}.csv"
@@ -166,7 +176,11 @@ if __name__ == "__main__":
         matching_matrix_z, noise_level=args.noise_level
     )
     G = matching_matrix_to_graph(matching_matrix_noisy, density=0.05)
-    module_list, module_list_relabeled = run_infomap(G, fcpriors, verbose=False)
+    module_list, module_list_relabeled = run_infomap(
+        G, 
+        fcpriors, 
+        verbose=False, 
+        normalize=args.normalize)
     ari = adjusted_rand_score(maxprob_fc, module_list)
     results.append(
         [args.noise_level, ari]
